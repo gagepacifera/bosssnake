1936fc8b80ec5e30a5f4cb6704b296c988464ed1
Gage Pacifera <gagepacifera@gmail.com>
2020-04-09 19:53:57 -0700

    remove commented code

--- analyzer/analyzer.js
+++ analyzer/analyzer.js
@@ -21,13 +21,10 @@ function init(app) {
 }
 
 function getGameIndex(gameId) {
-  // console.log(`gameId = ${gameId}`)
   try {
     let gameIndex = false
     games.forEach((game, i) => {
-      // console.log('getGameIndex: this games array item id is ', game.gameId)
       if (game.gameId === gameId) {
-        // console.log('getGameIndex: index match found!')
         gameIndex = i
       }
     })
@@ -42,13 +39,10 @@ function log(str, gameState = lastRecordedGameState) {
   try {
     if (gameState) {
       let gameIndex = getGameIndex(gameState.game.id)
-      // console.log(`log: ${str}, turn = ${gameState.turn}, game id = ${gameState.game.id}, game index = ${gameIndex}`)
-      // return `log: ${str}, turn = ${gameState.turn}, game index = ${gameIndex}`
       games[gameIndex].log[gameState.turn].push(str)
     }
   } catch (err) {
     console.error('log: err = ', err, 'gameState = ', gameState)
-    // return `log: error: ${err}`
   }
 }
 
@@ -56,13 +50,10 @@ function dir(obj, gameState = lastRecordedGameState) {
   try {
     if (gameState) {
       let gameIndex = getGameIndex(gameState.game.id)
-      // console.log(`log: ${str}, turn = ${gameState.turn}, game id = ${gameState.game.id}, game index = ${gameIndex}`)
-      // return `log: ${str}, turn = ${gameState.turn}, game index = ${gameIndex}`
       games[gameIndex].dir[gameState.turn].push(obj)
     }
   } catch (err) {
     console.error('dir: err = ', err, 'gameState = ', gameState)
-    // return `log: error: ${err}`
   }
 }
 
--- analyzer/public/src/app.js
+++ analyzer/public/src/app.js
@@ -6,12 +6,9 @@ var analyzerData;
 
 fetch('http://localhost:5000/analyze', { mode: "no-cors" })
   .then((response) => {
-    // console.log("response = ", response)
     return response.json()
   })
   .then((data) => {
-    // console.log("data = " + JSON.stringify(data))
-    // console.log("data = ", data)
     analyzerData = new Vue({
       el: '#wrapper',
       data: {
@@ -39,53 +36,9 @@ fetch('http://localhost:5000/analyze', { mode: "no-cors" })
       created: function () {
         window.addEventListener('keyup', this.handleKeyPress)
       },
-      computed: {
-        displayConsole: function() {
-          console.log('displayConsole')
-          try {
-            if (analyzerData && analyzerData.hasOwnProperty('games') && analyzerData.games) {
-              // console.log('we have analyzerData and games')
-              // console.log(analyzerData.games[analyzerData.currGame].log[analyzerData.currTurn][0])
-              var logs = analyzerData.games[analyzerData.currGame].log[analyzerData.currTurn];
-              var dirs = analyzerData.games[analyzerData.currGame].dir[analyzerData.currTurn];
-              // if (logs){
-              //   logs.forEach((l) => {
-              //     console.log(l)
-              //   })
-              // }
-              // if (dirs){
-              //   console.log('dir found!')
-              //   dirs.forEach((d) => {
-              //     console.log(d)
-              //     console.dir(d)
-              //   })
-              // }
-            //    // this.displayConsoleMessages()
-            //    // return `${this.currTurn}`
-            //    return analyzerData.games[analyzerData.currGame].log[analyzerData.currTurn]
-            }
-             // else {
-             //   console.log('displayConsole: analyzerData = ', analyzerData)
-             //   return `${this.currTurn}`
-             // }
-          } catch (err) {
-           console.error('displayConsole: err = ', err, 'analyzerData = ', analyzerData)
-          }
-        }
-      },
-      // watch: {
-      //   displayConsole: function(logsArr) {
-      //     try {
-      //       console.log(logsArr[0])
-      //     } catch (err) {
-      //       console.error('watch->displayConsole: err = ', err)
-      //     }
-      //   }
-      // }
       watch: {
         currTurn: function() {
           try {
-            // console.log(analyzerData.games[analyzerData.currGame].log[analyzerData.currTurn][0])
             return `${this.currTurn}`
           } catch (err) {
             console.error('watch->displayConsole: err = ', err)
